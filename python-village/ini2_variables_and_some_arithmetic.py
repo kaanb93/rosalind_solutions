e58a3e643eb897b6735b3bd679b55f41830868a9
Kaan Büyükaltay <52814388+kaanb93@users.noreply.github.com>
2022-08-24 11:15:14 +0300

    Update ini2_variables_and_some_arithmetic.py

--- python-village/ini2_variables_and_some_arithmetic.py
+++ python-village/ini2_variables_and_some_arithmetic.py
@@ -1,8 +1,8 @@
 # var1
-a = int(input("First Variable: "))
+a = int(input('First Variable: '))
 
 # var2
-b = int(input("Second Variable: "))
+b = int(input('Second Variable: '))
 
 # operation
 result = a**2 + b**2
