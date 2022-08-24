c1c6002f4b16b178e6f74624c73c3c47624e13d4
Kaan Büyükaltay <52814388+kaanb93@users.noreply.github.com>
2022-08-24 11:10:36 +0300

    Update dna_counting_dna_nucleotides.py

--- bioinformatics-stronghold/dna_counting_dna_nucleotides.py
+++ bioinformatics-stronghold/dna_counting_dna_nucleotides.py
@@ -1,8 +1,10 @@
-import collections
-import sys
+from collections import Counter
 
-file = open(sys.argv[1],"r")
-file = file.readlines()
-s=file[0]
-sdict=dict(collections.Counter(s))
-print(str(sdict["A"])+" "+str(sdict["C"])+" "+str(sdict["G"])+" "+str(sdict["T"]))
+# open downloaded dataset
+file = open("rosalind_dna.txt", "r")
+
+# save nucleotide counts to a dictionary
+nuc_dict = dict(Counter(file))
+
+# print results in the specified order
+print(str(nuc_dict["A"]), str(nuc_dict["C"]), str(nuc_dict["G"]), str(nuc_dict["T"]), sep=" ")
