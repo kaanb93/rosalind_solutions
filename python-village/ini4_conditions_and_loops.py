3030fa92f5d056b64c9cf93deb841eead55c8747
Kaan Büyükaltay <52814388+kaanb93@users.noreply.github.com>
2022-08-24 11:16:41 +0300

    Update ini4_conditions_and_loops.py

--- python-village/ini4_conditions_and_loops.py
+++ python-village/ini4_conditions_and_loops.py
@@ -1,12 +1,12 @@
 # var_a
-a = int(input("Variable a: "))
+a = int(input('Variable a: '))
 
 # var_b
-b = int(input("Variable b: "))
+b = int(input('Variable b: '))
 
 # operation
 sum = 0
-for n in range(a, b+1):
+for n in range(a, b + 1):
     if n % 2 == 1:
         sum += n
     else:
