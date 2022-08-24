0be3676c980c548ce1d31d88a549b5b5242c3f22
Kaan Büyükaltay <52814388+kaanb93@users.noreply.github.com>
2022-08-24 10:45:39 +0300

    Update ini3-strings-and-lists.py

--- python-village/ini3-strings-and-lists.py
+++ python-village/ini3-strings-and-lists.py
@@ -1,6 +1,18 @@
-a=71
-b=79
-c=179
-d=185
-s="BoFyd9sErqpZVWoFPR4xfOkFT0qrfPuMfWVHeJrz4A1gNJM47NCJImwQAlGi2Kgpoq3nruxCoregonusHZbDwApdevNT0sY2d2kVhXq1uCuB9vbUBaDMNJ3GCb3tQCJ9AFp8X4QumPHwFLKFdx7MEkjdQoF2m0bZwmzdWB5gttciOwx0hujzenobiaW1Nwq7U8."
-print(s[a:b+1]+" "+s[c:d+1])
+# string variable
+s = str(input("String Variable: "))
+
+# var_a
+a = int(input("Variable a: "))
+
+# var_b
+b = int(input("Variable b: "))
+
+# var_c
+c = int(input("Variable c: "))
+
+# var_d
+d = int(input("Variable d: "))
+
+# operation
+result = s[a:b+1]+" "+s[c:d+1]
+print(result)
