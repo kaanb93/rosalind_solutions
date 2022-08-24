37854c669aef65b0a98abb62f6328a6a7e4b13c8
Kaan Büyükaltay <52814388+kaanb93@users.noreply.github.com>
2022-08-24 10:55:06 +0300

    Update ini5-working-with-files.py

--- python-village/ini5-working-with-files.py
+++ python-village/ini5-working-with-files.py
@@ -1,9 +1,12 @@
-import sys
+# open downloaded dataset
+file = open("rosalind_ini5.txt", "r")
 
-file=open(sys.argv[1],"r")
-lines=file.readlines()
+# read each line to make it into a list
+lines = file.readlines()
+
+# loop through the list to get even numbered lines
 for i,line in enumerate(lines):
-    if i%2==0:
+    if i % 2 == 0:
         continue
     else:
         print(line)
