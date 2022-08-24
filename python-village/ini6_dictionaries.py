5755398ba698363c4019f47db11de3b64a5ca656
Kaan Büyükaltay <52814388+kaanb93@users.noreply.github.com>
2022-08-24 11:17:30 +0300

    Update ini6_dictionaries.py

--- python-village/ini6_dictionaries.py
+++ python-village/ini6_dictionaries.py
@@ -1,14 +1,14 @@
 from collections import Counter
 
 # open downloaded dataset
-s = open("rosalind_ini6.txt", "r")
+s = open('rosalind_ini6.txt', 'r')
 
 # split dataset to make it into a list
-slist = s.split(" ")
+slist = s.split(' ')
 
 # Counter does all the work itself, just need to store it as a dictionary
 sdict = dict(Counter(slist))
 
 # print results
 for key in sdict.keys():
-    print(key + " " + str(sdict[key]))
+    print(key + ' ' + str(sdict[key]))
